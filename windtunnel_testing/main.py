73d165a6a6afe5cad74ccac5e939826797856e0d
Bartosz Jemioł <ekstrabartek@gmail.com>
2021-12-10 21:34:39 +0100

    Making graphs nice

--- windtunnel_testing/main.py
+++ windtunnel_testing/main.py
@@ -5,7 +5,7 @@ data1 = "data/corr_test.txt"
 data2 = "data/naca642015a_no_vis.txt"
 data3 = "data/naca642015a_vic.txt"
 
-list1 = np.genfromtxt(data1, dtype='float', skip_header=2, usecols=(1, 2, 3, 4))
+list1 = np.genfromtxt(data1, dtype='float', skip_header=2, skip_footer=10, usecols=(1, 2, 3, 4))
 list2 = np.genfromtxt(data2, dtype='float', skip_header=12, usecols=(0, 1, 2, 4))
 list3 = np.genfromtxt(data3, dtype='float', skip_header=12, usecols=(0, 1, 2, 4))
 
@@ -25,5 +25,18 @@ cl_no_vis = list2[:, 1].tolist()
 cd_no_vis = list2[:, 2].tolist()
 cm_no_vis = list2[:, 3].tolist()
 
-plt.plot(alpha_no_vis, cl_no_vis)
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.axhline(color='black')
+ax1.axvline(color='black')
+ax1.set_xlim(-5, 20)
+ax1.set_ylim(-0.5, 1.25)
+ax1.set_xticks(np.arange(-5, 21, 2.5))
+ax1.set_yticks(np.arange(-0.5, 1.26, 0.25))
+ax1.set_xlabel("X label")
+ax1.set_ylabel("Y label")
+plt.plot(alpha_test, cl_test, marker='+')
+plt.plot(alpha_vis, cl_vis)
+plt.title("Title")
+plt.grid(True)
 plt.show()
