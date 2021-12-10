3ac2a58a35a9bf8bab266e0df6ea31b81c9c0920
Bartosz Jemioł <ekstrabartek@gmail.com>
2021-12-10 23:13:12 +0100

    Added drag polar

--- windtunnel_testing/main.py
+++ windtunnel_testing/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
 
@@ -26,7 +27,7 @@ cl_no_vis = list2[:, 1].tolist()
 cd_no_vis = list2[:, 2].tolist()
 cm_no_vis = list2[:, 3].tolist()
 
-'''
+
 # Lift plot
 
 fig = plt.figure()
@@ -43,12 +44,13 @@ ax1.xaxis.set_major_formatter(StrMethodFormatter(u'{x:.1f}°'))
 plt.plot(alpha_test, cl_test, marker='x', label="Real world")
 plt.plot(alpha_vis, cl_vis, label="XFOIL with viscosity")
 plt.plot(alpha_no_vis, cl_no_vis, label="XFOIL without viscosity")
+plt.plot([-math.degrees(1), math.degrees(1)], [-2 * math.pi, 2 * math.pi], label="ideal lift")
 plt.title(r'Lift Coefficient vs. Angle of attack')
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
 plt.show()
-'''
+
 '''
 # Drag plot
 
@@ -95,4 +97,26 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 '''
+'''
+# Drag polar
 
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.axhline(color='black')
+ax1.axvline(color='black', lw=3)
+ax1.set_xlim(0, 0.1)
+ax1.set_ylim(-0.5, 1.25)
+ax1.set_xticks(np.arange(0, 0.11, 0.01))
+ax1.set_yticks(np.arange(-0.5, 1.26, 0.25))
+ax1.set_ylabel(r'Lift Coefficient ($c_l$)')
+ax1.set_xlabel(r'Drag Coefficient ($c_d$)')
+# ax1.xaxis.set_major_formatter(StrMethodFormatter(u'{x:.1f}°'))
+plt.plot(cd_test, cl_test, marker='x', label="Real world")
+plt.plot(cd_vis, cl_vis, label="XFOIL with viscosity")
+# plt.plot(cd_no_vis, cl_no_vis, label="XFOIL without viscosity") # no drag so need for it being included
+plt.title(r'Lift Coefficient vs. Drag coefficient')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
+'''
